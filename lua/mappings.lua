2f7a009d977016e1e61007d15df9c5a3b8225f5b
Nicholas Konar <nkonar@protonmail.com>
2025-03-02 14:46:12 -0500

    Toggle terms with <leader>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,24 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- toggle term with <leader> (swaps defaults from ~/.local/share/nvim/lazy/NvChad/lua/nvchad/mappings.lua)
+map("n", "<A-h>", function()
+  require("nvchad.term").new { pos = "sp" }
+end, { desc = "terminal new horizontal term" })
+
+map("n", "<A-v", function()
+  require("nvchad.term").new { pos = "vsp" }
+end, { desc = "terminal new vertical term" })
+
+map({ "n", "t" }, "<leader>h", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<leader>v", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggleable vertical term" })
+
+map({ "n", "t" }, "<leader>i", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
