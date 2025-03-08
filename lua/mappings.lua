68bd212fe6c81fc5b5aa85ba0d94995f1b46f27e
Nicholas Konar <nkonar@protonmail.com>
2025-03-08 14:40:02 -0500

    Disable non-toggle term

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,24 +9,19 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
--- toggle term with <leader> (swaps defaults from ~/.local/share/nvim/lazy/NvChad/lua/nvchad/mappings.lua)
-map("n", "<A-h>", function()
-  require("nvchad.term").new { pos = "sp" }
-end, { desc = "terminal new horizontal term" })
-
-map("n", "<A-v", function()
-  require("nvchad.term").new { pos = "vsp" }
-end, { desc = "terminal new vertical term" })
+-- remove defaults from ~/.local/share/nvim/lazy/NvChad/lua/nvchad/mappings.lua
+vim.keymap.del("n", "<leader>h")
+vim.keymap.del("n", "<leader>v")
 
 -- shared id so session persists across orientations
-map({ "n", "t" }, "<leader>h", function()
+map({ "n", "t" }, "<A-h>", function()
   require("nvchad.term").toggle { pos = "sp", id = "shared" }
 end, { desc = "terminal toggleable horizontal term" })
 
-map({ "n", "t" }, "<leader>v", function()
+map({ "n", "t" }, "<A-v>", function()
   require("nvchad.term").toggle { pos = "vsp", id = "shared" }
 end, { desc = "terminal toggleable vertical term" })
 
-map({ "n", "t" }, "<leader>i", function()
+map({ "n", "t" }, "<A-i>", function()
   require("nvchad.term").toggle { pos = "float", id = "shared" }
 end, { desc = "terminal toggle floating term" })
