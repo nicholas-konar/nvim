5f1a8a1538b9a5a9a2842cd83565c905ceb43f77
Nicholas Konar <nkonar@protonmail.com>
2025-04-15 13:42:14 -0400

    Cleanup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -23,9 +23,3 @@ lspconfig.ts_ls.setup {
   capabilities = nvlsp.capabilities,
   root_dir = util.root_pattern("jsconfig.json", "tsconfig.json", ".git"),
 }
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
