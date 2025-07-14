b9c72d2d8fdf811ec04eb8d45704e25c0c57db2e
Nicholas Konar <nkonar@protonmail.com>
2025-07-13 22:04:22 -0400

    vim formatting rules

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,8 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
