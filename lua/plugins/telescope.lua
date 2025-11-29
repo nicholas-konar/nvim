7cd131f36da84db24f736fc98f6ed90089052858
Nicholas Konar <nkonar@protonmail.com>
2025-11-29 15:48:16 -0500

    add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function() 
+        require('telescope').setup({
+            pickers = {
+                colorscheme = {
+                    enable_preview = true
+                }
+            }
+        })
+      end
+    }
