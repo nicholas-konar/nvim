33183a5cc04d4bc31886068bdeb62c03d61c6896
Nicholas Konar <nkonar@protonmail.com>
2025-11-29 19:51:06 -0500

    add mason & tooling

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"black",
+			},
+			run_on_start = true,
+			start_delay = 3000,
+			debounce_hours = 12,
+		},
+	},
+}
