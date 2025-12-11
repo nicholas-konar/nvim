469e7083453a753bd385188aee1c8c46b3d82136
Nicholas Konar <nkonar@protonmail.com>
2025-12-11 11:07:45 -0500

    mini spring

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,9 @@ return {
 		"craftzdog/solarized-osaka.nvim",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("solarized-osaka")
-		end,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("solarized-osaka")
+		-- end,
 	},
 	{
 		"rebelot/kanagawa.nvim",
