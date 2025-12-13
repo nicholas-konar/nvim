05f38847a98025f80a06f65c38dd2d50b610b255
Nicholas Konar <nkonar@protonmail.com>
2025-12-13 11:41:10 -0500

    setup custom keymapping

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+require("mappings")
 
 require("lazy").setup({
 	spec = {
---
+++ lua/config/mappings.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", ";", ":", { desc = "enter cmd mode" })
