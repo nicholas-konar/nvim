432d429aa567636d59988d720aba774bab5d3e41
Nicholas Konar <nkonar@protonmail.com>
2025-12-14 01:56:31 -0500

    add mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,9 +2,19 @@ return {
 	{
 		"nvim-mini/mini.nvim",
 		version = false,
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("mini.files").open()
+				end,
+				desc = "open minifiles",
+			},
+		},
 		config = function()
 			vim.o.background = "dark"
 			vim.cmd.colorscheme("minispring")
+			require("mini.files").setup()
 		end,
 	},
 }
