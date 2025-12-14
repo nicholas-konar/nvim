89474fd0ba6a1128bb2b9fb0aa3c15b3d7d378ce
Nicholas Konar <nkonar@protonmail.com>
2025-12-14 10:59:37 -0500

    move mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,19 +2,7 @@ return {
 	{
 		"nvim-mini/mini.nvim",
 		version = false,
-		keys = {
-			{
-				"<leader>e",
-				function()
-					require("mini.files").open()
-				end,
-				desc = "open minifiles",
-			},
-		},
 		config = function()
-			-- vim.o.background = "dark"
-			-- vim.cmd.colorscheme("minispring")
-			require("mini.files").setup()
 			require("mini.comment").setup({
 				mappings = {
 					comment = "<leader>cc",
