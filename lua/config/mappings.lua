aa58aceb2edc9d4f71b1c5d55ff5ebf775145463
Nicholas Konar <nkonar@protonmail.com>
2025-12-15 01:22:30 -0500

    move telescope mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", ";", ":", { desc = "enter cmd mode" })
+-- vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Telescope live grep" })
+-- vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Telescope buffers" })
+-- vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Telescope help tags" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,36 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "v0.2.0",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Telescope find files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			desc = "Telescope live grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			desc = "Telescope buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+			desc = "Telescope help tags",
+		},
+	},
 	config = function()
 		require("telescope").setup({
 			pickers = {
