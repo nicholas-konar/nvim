5a7e1d76c999fd2c8ffe9c48013ed587aa933059
Nicholas Konar <nkonar@protonmail.com>
2026-01-01 18:36:24 -0500

    window nav mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -2,7 +2,18 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", ";", ":", { desc = "enter cmd mode" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { silent = true })
 
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<leader>x", "<cmd>bdelete<CR>", { desc = "Close buffer" })
+
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
+	desc = "LSP Rename",
+})
+
+-- Window nav
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Window right" })
