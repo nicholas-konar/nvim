3a64d72f4c11b3deebdd58bdb992d4b59e25d4c3
Nicholas Konar <nkonar@protonmail.com>
2026-01-03 23:06:31 -0500

    toggleterm refactor

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,28 +3,33 @@ return {
 	version = "*",
 	cmd = { "ToggleTerm", "TermExec" },
 	keys = {
+		{ "<leader>\\", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm (floating)" },
 		{ "<M-\\>", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm (floating)" },
 		{ "<M-h>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "ToggleTerm (horizontal)" },
 		{ "<M-v>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "ToggleTerm (vertical)" },
 	},
 	opts = {
-		direction = "float", -- default direction for :ToggleTerm / <C-\>
+		direction = "float",
 		start_in_insert = true,
+		persist_mode = false, -- if toggled off in normal mode, toggle on in terminal mode
 		insert_mappings = true,
 		terminal_mappings = true,
-		shade_terminals = true,
+		shade_terminals = false,
 		close_on_exit = true,
 		float_opts = {
 			border = "rounded",
 			winblend = 0,
+			height = math.floor(vim.o.lines * 0.8),
+			width = math.floor(vim.o.columns * 0.95),
 		},
 		size = function(term)
 			if term.direction == "horizontal" then
 				return 15
 			elseif term.direction == "vertical" then
 				return math.floor(vim.o.columns * 0.4)
+			else
+				return 20
 			end
-			return 20
 		end,
 	},
 	config = function(_, opts)
@@ -36,12 +41,14 @@ return {
 				local opts = { buffer = true, silent = true }
 
 				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<Esc><Esc>", "<cmd>ToggleTerm<cr>", opts)
 
 				vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
 				vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
 				vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
 				vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
 
+				-- redundant keymaps scoped to terminal for toggling off while still in terminal mode
 				vim.keymap.set("t", "<M-\\>", "<cmd>ToggleTerm direction=float<cr>", opts)
 				vim.keymap.set("t", "<M-h>", "<cmd>ToggleTerm direction=horizontal<cr>", opts)
 				vim.keymap.set("t", "<M-v>", "<cmd>ToggleTerm direction=vertical<cr>", opts)
