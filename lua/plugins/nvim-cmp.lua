7e7b91677182bd4abb7f38eb884c906612f7adb0
Nicholas Konar <nkonar@protonmail.com>
2026-01-06 16:44:28 -0500

    feat: nvim-cmp

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,143 @@
+-- lua/plugins/cmp.lua
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		-- completion sources
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lua",
+
+		-- snippets
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	opts = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		-- load VS Code–style snippet collections
+		pcall(function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end)
+
+		-- icons for completion item kinds
+		local kind_icons = {
+			Text = "󰉿",
+			Method = "󰆧",
+			Function = "󰆧",
+			Constructor = "",
+			Field = "󰜢",
+			Variable = "󰀫",
+			Class = "󰠱",
+			Interface = "",
+			Module = "",
+			Property = "󰜢",
+			Unit = "󰑭",
+			Value = "󰎠",
+			Enum = "",
+			Keyword = "󰌋",
+			Snippet = "",
+			Color = "󰏘",
+			File = "󰈙",
+			Reference = "󰈇",
+			Folder = "󰉋",
+			EnumMember = "",
+			Constant = "󰏿",
+			Struct = "󰙅",
+			Event = "",
+			Operator = "󰆕",
+			TypeParameter = "󰊄",
+		}
+
+		-- labels for completion sources
+		local source_labels = {
+			nvim_lsp = "[LSP]",
+			nvim_lua = "[API]",
+			luasnip = "[Snip]",
+			buffer = "[Buf]",
+			path = "[Path]",
+		}
+
+		return {
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+
+			preselect = cmp.PreselectMode.Item,
+
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+
+			-- bordered completion and documentation windows
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+
+			mapping = cmp.mapping.preset.insert({
+				-- navigate completion items and snippet jumps
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+				-- documentation and control
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				cmp.mapping.scroll_docs(""),
+			}),
+
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+			},
+
+			formatting = {
+				fields = { "menu", "abbr", "kind" },
+				format = function(entry, vim_item)
+					vim_item.kind = (kind_icons[vim_item.kind] or "") .. " " .. vim_item.kind
+
+					-- prefer LSP-provided detail when available
+					local detail = entry.completion_item.detail
+					if detail and detail ~= "" then
+						vim_item.menu = detail
+					else
+						vim_item.menu = source_labels[entry.source.name] or ""
+					end
+
+					return vim_item
+				end,
+			},
+		}
+	end,
+}
