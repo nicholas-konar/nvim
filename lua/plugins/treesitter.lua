7a579ba2b36ff617295c36b9957a91a31892516b
Nicholas Konar <nkonar@protonmail.com>
2026-01-12 23:59:31 -0500

    feat: treesitter-textobjects
    wip

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,34 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	init = function()
-		require("nvim-treesitter").install({ "typescript", "javascript", "python", "lua" })
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		init = function()
+			require("nvim-treesitter").install({ "typescript", "javascript", "python", "lua" })
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		opts = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+				selection_modes = {
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				include_surrounding_whitespace = true,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter-textobjects").setup(opts)
+		end,
+	},
 }
