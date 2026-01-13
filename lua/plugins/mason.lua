084befbc3bc3ea8bb33b696b7a1999f78a08a130
Nicholas Konar <nkonar@protonmail.com>
2026-01-13 17:40:26 -0500

    feat(mason): typescript lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,7 +8,7 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls" },
+			ensure_installed = { "lua_ls", "ts_ls" },
 		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
