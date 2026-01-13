1a355885c358f2124aac81c35ebf05fbcd6a69f4
Nicholas Konar <nkonar@protonmail.com>
2026-01-13 17:59:23 -0500

    feat(lsp): add mappings for go to def & impl

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -42,7 +42,9 @@ map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
 
 -- LSP
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+map("n", "gd", vim.lsp.buf.implementation, { desc = "LSP: go to implementation" })
+map("n", "gD", vim.lsp.buf.definition, { desc = "LSP: go to definition" })
 
 -- Telescope
 map("n", "<leader>ff", "<cmd>Telescope git_files<CR>", { desc = "Telescope find files" })
