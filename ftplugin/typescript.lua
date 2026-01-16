629e2cebc7558194bd87b24a4863ebafe515f569
Nicholas Konar <nkonar@protonmail.com>
2026-01-15 20:44:41 -0500

    feat(nvim): indentation behavior tweaks

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -2,3 +2,7 @@ vim.treesitter.start()
 
 vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo[0][0].foldmethod = "manual"
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
