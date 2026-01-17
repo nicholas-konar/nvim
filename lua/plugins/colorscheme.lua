0b7f4b03635d3c707ba7844a2ff5fe9ecdd5cd96
Nicholas Konar <nkonar@protonmail.com>
2026-01-17 15:58:24 -0500

    refactor(colorscheme): convert values to solarized-osaka vars

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,36 +7,36 @@ return {
 			transparent = false,
 			dim_inactive = true,
 			on_highlights = function(hl, c)
-				hl.NormalNC = { bg = "#001116" }
+				hl.NormalNC = { bg = c.bg }
 
-				hl.LineNr = { fg = "#586e75" } -- base01 (muted)
-				hl.LineNrAbove = { fg = "#073642" } -- base02 (very subtle)
-				hl.LineNrBelow = { fg = "#073642" } -- base02
-				hl.CursorLineNr = { fg = "#93a1a1", bold = true } -- base1
+				hl.LineNr = { fg = c.base01 }
+				hl.LineNrAbove = { fg = c.base02 }
+				hl.LineNrBelow = { fg = c.base02 }
+				hl.CursorLineNr = { fg = c.base1, bold = true }
 
 				-- nvim-cmp groups
-				hl.CmpItemAbbrMatch = { fg = "#b58900", bold = true }
-				hl.CmpItemAbbrMatchFuzzy = { fg = "#b58900", bold = true }
-				hl.CmpItemKindText = { fg = "#2aa198" }
-				hl.CmpItemKindVariable = { fg = "#268bd2" }
-				hl.CmpItemKindClass = { fg = "#b58900" }
+				hl.CmpItemAbbrMatch = { fg = c.yellow500, bold = true }
+				hl.CmpItemAbbrMatchFuzzy = { fg = c.yellow500, bold = true }
+				hl.CmpItemKindText = { fg = c.cyan500 }
+				hl.CmpItemKindVariable = { fg = c.blue500 }
+				hl.CmpItemKindClass = { fg = c.yellow500 }
 				hl.CmpItemKindFunction = { fg = "#cb4b16" }
 				hl.CmpItemKindMethod = { fg = "#d33682" }
-				hl.CmpItemKindKeyword = { fg = "#859900" }
+				hl.CmpItemKindKeyword = { fg = c.green500 }
 				hl.CmpItemKindField = { fg = "#6c71c4" }
-				hl.CmpItemKindProperty = { fg = "#2aa198" }
-				hl.CmpItemKindInterface = { fg = "#268bd2" }
+				hl.CmpItemKindProperty = { fg = c.cyan500 }
+				hl.CmpItemKindInterface = { fg = c.blue500 }
 				hl.CmpItemKindModule = { fg = "#d33682" }
 				hl.CmpItemKindStruct = { fg = "#cb4b16" }
-				hl.CmpItemKindEnum = { fg = "#859900" }
-				hl.CmpItemKindConstant = { fg = "#b58900" }
+				hl.CmpItemKindEnum = { fg = c.green500 }
+				hl.CmpItemKindConstant = { fg = c.yellow500 }
 
 				-- treesitter groups (must be accessed via string key)
 				hl["@variable.typescript"] = { fg = "#9FACAC" }
-				hl["@variable.builtin.typescript"] = { fg = "#859800" }
-				hl["@variable.builtin.tsx"] = { fg = "#859800" }
-				hl["@keyword.import.typescript"] = { fg = "#859800" }
-				hl["@keyword.import.tsx"] = { fg = "#859800" }
+				hl["@variable.builtin.typescript"] = { fg = c.green500 }
+				hl["@variable.builtin.tsx"] = { fg = c.green500 }
+				hl["@keyword.import.typescript"] = { fg = c.green500 }
+				hl["@keyword.import.tsx"] = { fg = c.green500 }
 
 				-- lsp global namespaces (e.g. jest)
 				hl["@lsp.type.namespace.typescript"] = { fg = "#D33682" }
