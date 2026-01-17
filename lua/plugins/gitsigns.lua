f52118bcbeee98b792d9ca93dc5093ff8ce11546
Nicholas Konar <nkonar@protonmail.com>
2026-01-17 16:47:43 -0500

    feat(gitsigns): ghost text inline blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 200,
+				ignore_whitespace = true,
+			},
+		})
+		vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
+	end,
 }
