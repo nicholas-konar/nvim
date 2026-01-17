1c8850effc95f73ba61e59bdecac19a338181cce
Nicholas Konar <nkonar@protonmail.com>
2026-01-17 11:16:55 -0500

    refactor(mini.tab): semantic links

--- lua/plugins/mini.tabline.lua
+++ lua/plugins/mini.tabline.lua
@@ -3,5 +3,14 @@ return {
 	version = false,
 	config = function()
 		require("mini.tabline").setup()
+		local hl = vim.api.nvim_set_hl
+
+		hl(0, "MiniTablineCurrent", { link = "TabLineCurrent" })
+		hl(0, "MiniTablineVisible", { link = "TabLineVisible" })
+		hl(0, "MiniTablineHidden", { link = "TabLineHidden" })
+
+		hl(0, "MiniTablineModifiedCurrent", { link = "TabLineModifiedCurrent" })
+		hl(0, "MiniTablineModifiedVisible", { link = "TabLineModifiedVisible" })
+		hl(0, "MiniTablineModifiedHidden", { link = "TabLineModifiedHidden" })
 	end,
 }
