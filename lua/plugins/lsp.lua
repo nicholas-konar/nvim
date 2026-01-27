d326ccbf5570439509c2472f37c8e34a6daf4ab1
Nicholas Konar <nkonar@protonmail.com>
2026-01-27 12:06:08 -0500

    feat(lsp): configure workspace ts server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,56 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
+	"neovim/nvim-lspconfig",
+	config = function()
+		local util = require("lspconfig.util")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		-- Prefer workspace TypeScript when available
+		local function get_tsdk(root_dir)
+			return vim.fs.find("node_modules/typescript/lib", {
+				path = root_dir,
+				upward = true,
+			})[1]
+		end
+
+		local servers = {
+			lua_ls = {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
 						},
 					},
 				},
-				ts_ls = {},
-			}
+			},
 
-			for name, config in pairs(servers) do
-				vim.lsp.config(name, config)
-				vim.lsp.enable(name)
-			end
-		end,
-	},
+			ts_ls = {
+				capabilities = capabilities,
+
+				root_dir = function(bufnr, on_dir)
+					local fname = vim.api.nvim_buf_get_name(bufnr)
+					local root =
+						util.root_pattern("tsconfig.json", "tsconfig.*.json", "jsconfig.json", "package.json", ".git")(
+							fname
+						)
+
+					on_dir(root or vim.fn.getcwd())
+				end,
+
+				on_new_config = function(new_config, new_root_dir)
+					local tsdk = get_tsdk(new_root_dir)
+					if tsdk then
+						new_config.init_options = new_config.init_options or {}
+						new_config.init_options.tsserver = new_config.init_options.tsserver or {}
+						new_config.init_options.tsserver.tsdk = tsdk
+					end
+				end,
+			},
+		}
+
+		for name, config in pairs(servers) do
+			vim.lsp.config(name, config)
+			vim.lsp.enable(name)
+		end
+	end,
 }
