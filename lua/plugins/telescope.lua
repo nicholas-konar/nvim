5a602375ef0df73a2810a1e6178e3c015ca26769
Nicholas Konar <nkonar@protonmail.com>
2026-01-30 09:58:12 -0500

    feat(telescope): use latest version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	version = false,
 	dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
 	cmd = "Telescope",
 	config = function()
