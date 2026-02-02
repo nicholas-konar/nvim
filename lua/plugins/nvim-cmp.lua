70e8d11163b6f23f00a63c852681a163e06adc35
Nicholas Konar <nkonar@protonmail.com>
2026-02-02 15:58:01 -0500

    fix(telescope): disable nvim-cmp <C-Space> mapping
    Telescope refine search function was being shadowed

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -120,7 +120,7 @@ return {
 				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
+				-- ["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				cmp.mapping.scroll_docs(""),
 			}),
