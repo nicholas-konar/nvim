58d3a22d0995a753b4d269f86b68372d379d20d5
Nicholas Konar <nkonar@protonmail.com>
2026-02-18 13:03:16 -0500

    style(colorscheme): convert hex values to solarized osaka variables

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -23,26 +23,26 @@ return {
 				hl.CmpItemKindText = { fg = c.cyan500 }
 				hl.CmpItemKindVariable = { fg = c.blue500 }
 				hl.CmpItemKindClass = { fg = c.yellow500 }
-				hl.CmpItemKindFunction = { fg = "#cb4b16" }
-				hl.CmpItemKindMethod = { fg = "#d33682" }
+				hl.CmpItemKindFunction = { fg = c.orange500 }
+				hl.CmpItemKindMethod = { fg = c.magenta500 }
 				hl.CmpItemKindKeyword = { fg = c.green500 }
-				hl.CmpItemKindField = { fg = "#6c71c4" }
+				hl.CmpItemKindField = { fg = c.violet500 }
 				hl.CmpItemKindProperty = { fg = c.cyan500 }
 				hl.CmpItemKindInterface = { fg = c.blue500 }
-				hl.CmpItemKindModule = { fg = "#d33682" }
-				hl.CmpItemKindStruct = { fg = "#cb4b16" }
+				hl.CmpItemKindModule = { fg = c.magenta500 }
+				hl.CmpItemKindStruct = { fg = c.orange500 }
 				hl.CmpItemKindEnum = { fg = c.green500 }
 				hl.CmpItemKindConstant = { fg = c.yellow500 }
 
 				-- treesitter groups (must be accessed via string key)
-				hl["@variable.typescript"] = { fg = "#9FACAC" }
+				hl["@variable.typescript"] = { fg = c.base0 }
 				hl["@variable.builtin.typescript"] = { fg = c.green500 }
 				hl["@variable.builtin.tsx"] = { fg = c.green500 }
 				hl["@keyword.import.typescript"] = { fg = c.green500 }
 				hl["@keyword.import.tsx"] = { fg = c.green500 }
 
 				-- lsp global namespaces (e.g. jest)
-				hl["@lsp.type.namespace.typescript"] = { fg = "#D33682" }
+				hl["@lsp.type.namespace.typescript"] = { fg = c.magenta500 }
 
 				-- mini.tabline hooks into these links
 				hl.TabLineCurrent = { bg = c.base03 }
