f2165186bf79a9187a8958ede872b4c61149dc25
Nicholas Konar <nkonar@protonmail.com>
2026-02-19 03:36:12 -0500

    feat(notifications): telescope notis view & style tweaks

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -2,6 +2,24 @@ return {
 	"rcarriga/nvim-notify",
 	lazy = false,
 	config = function()
-		vim.notify = require("notify")
+		local notify = require("notify")
+
+		notify.setup({
+			max_width = 60,
+			max_height = 15,
+			position = "top_right",
+			timeout = 3000,
+			stages = "fade_in_slide_out",
+		})
+
+		vim.notify = notify
+
+		-- View notification history with Telescope
+		vim.keymap.set("n", "<leader>fn", function()
+			require("telescope").extensions.notify.notify()
+		end, { desc = "Find notifications" })
 	end,
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
 }
